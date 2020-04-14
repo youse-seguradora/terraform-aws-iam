4d22120801068eba565a6c973559ab9440affdcf
Daniel Cristian <danielcrisap@gmail.com>
2020-04-14 12:43:17 -0300

    Adding wait-for-it

--- examples/iam-account/main.tf
+++ examples/iam-account/main.tf
@@ -8,25 +8,7 @@ provider "aws" {
   skip_requesting_account_id  = true
 
   endpoints {
-    apigateway     = "http://localstack:4567"
-    cloudformation = "http://localstack:4581"
-    cloudwatch     = "http://localstack:4582"
-    dynamodb       = "http://localstack:4569"
-    es             = "http://localstack:4578"
-    firehose       = "http://localstack:4573"
     iam            = "http://localstack:4593"
-    kinesis        = "http://localstack:4568"
-    lambda         = "http://localstack:4574"
-    route53        = "http://localstack:4580"
-    redshift       = "http://localstack:4577"
-    s3             = "http://localstack:4572"
-    secretsmanager = "http://localstack:4584"
-    ses            = "http://localstack:4579"
-    sns            = "http://localstack:4575"
-    sqs            = "http://localstack:4576"
-    ssm            = "http://localstack:4583"
-    stepfunctions  = "http://localstack:4585"
-    sts            = "http://localstack:4592"
   }
 }
 
